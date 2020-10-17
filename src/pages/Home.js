0e901bdb96594a85f6116bf2465023d9a9a2b270
mayuri <pmayuri124@gmail.com>
2020-10-17 23:39:48 +0530

    search bar completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import  './Home.css';
 import { Link } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
+import Search from './Search';
 
 
 function Home() {
@@ -27,9 +28,16 @@ function Home() {
                     <Avatar />
                  </div>
             </div>
-            <div className="home__body">
-
-            </div>
+                <div className="home__body">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+                    alt=""
+                />
+                <div className="home__inputContainer">
+                    {/*search */}
+                    <Search />
+                </div>
+                </div>
         </div>
     )
 }
